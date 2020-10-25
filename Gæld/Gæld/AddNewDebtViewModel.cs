e86459fea0c9f7d14f44d06ac2633ff62d4afed3
Ermin Obradovac <au592117@uni.au.dk>
2020-10-25 16:39:35 +0100

    Working on it

---
+++ Gæld/Gæld/AddNewDebtViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace Gæld
+{
+    class AddNewDebtViewModel
+    {
+        public ICommand _Cancel;
+
+        public ICommand Cancel
+        {
+            get
+            {
+                return _Cancel ?? (_Cancel = new DelegateCommand(() =>
+                {
+                    
+                }
+                ));
+            }
+        }
+    }
+}
