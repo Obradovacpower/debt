08d79d7fc39ab7364acefce5c0c47beda0bee176
Sebastian <sebastian@nielsen2u.dk>
2020-10-25 16:22:17 +0100

    migration with data

---
+++ Gæld/Gæld/Classes/UnitOfWork.cs
@@ -0,0 +1,54 @@
+﻿using Gæld.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gæld.Database.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+
+namespace Gæld.Classes
+{
+    class UnitOfWork 
+    {
+        protected readonly Repository<Person> persons;
+        protected readonly Repository<Debt> debts;
+        public UnitOfWork(Repository<Person> p, Repository<Debt> d)
+        {
+            persons = p;
+            debts = d;
+        }
+
+        public void AddPerson(string name)
+        {
+            Person p = new Person();
+            p.Name = name;
+            p.Debts = new List<Debt>();
+
+            persons.Add(p);
+        }
+        public void AddDebt(int amount, int personId)
+        {
+            Debt d = new Debt();
+            d.Amount = amount;
+            d.Date = DateTime.Now;
+            debts.Add(d);
+
+            Person p = persons.Get(personId);
+            p.Debts.Add(d);
+            persons.Update(p);
+        }
+        public void AddDebt(int amount, Person p)
+        {
+            Debt d = new Debt();
+            d.Amount = amount;
+            d.Date = DateTime.Now;
+            debts.Add(d);
+
+            p.Debts.Add(d);
+            persons.Update(p);
+        }
+
+    }
+}
--- Gæld/Gæld/Database/DebtBookContext.cs
+++ Gæld/Gæld/Database/DebtBookContext.cs
@@ -12,8 +12,7 @@ namespace Gæld.Database
     {
         protected override void OnConfiguring(DbContextOptionsBuilder ob)
         {
-            //connect here
-            //ob.UseSqlServer(@"Data Source=localhost,1433;Database=vareDatabase;User ID=SA;Password=SecPass1;");
+            ob.UseSqlServer(@"Data Source=localhost,1433;Database=testDB;User ID=SA;Password=SecPass1;");
         }
         public DbSet<Person> Persons { get; set; }
         public DbSet<Debt> Debts { get; set; }
@@ -25,6 +24,21 @@ namespace Gæld.Database
                 .HasOne(a => a.Person)
                 .WithMany(b => b.Debts)
                 .HasForeignKey(e => e.PersonId);
+
+            modelBuilder.Entity<Person>()
+                .HasData(
+                new Person { Id = 1, Name = "Carsten" },
+                new Person { Id = 2, Name = "Sebastian" },
+                new Person { Id = 3, Name = "Ermin Jensen"},
+                new Person { Id = 4, Name = "Rayquaza" });
+            modelBuilder.Entity<Debt>()
+                .HasData(
+                new Debt { Id = 1, PersonId = 1, Amount = -30, Date = DateTime.Now },
+                new Debt { Id = 2, PersonId = 1, Amount = -15, Date = DateTime.Now },
+                new Debt { Id = 3, PersonId = 1, Amount = 19, Date = DateTime.Now },
+                new Debt { Id = 4, PersonId = 2, Amount = 5000, Date = DateTime.Now},
+                new Debt { Id = 5, PersonId = 3, Amount = -5000, Date = DateTime.Now.AddDays(-30)},
+                new Debt { Id = 6, PersonId = 4, Amount = 1234, Date = DateTime.Now.AddDays(-15)});
 
         }
     }
