e771464d819e9cba8dd9f6045349cf272deddda4
Ermin Obradovac <au592117@uni.au.dk>
2020-10-25 14:40:28 +0100

    Added stackpanel

---
+++ Gæld/Gæld/DebtList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace Gæld
+{
+    class DebtList: BindableBase
+    {
+        public ICommand _NewDebt;
+
+        public ICommand NewDebt
+        {
+            get
+            {
+                return _NewDebt ?? (_NewDebt = new DelegateCommand(() =>
+                {
+
+                }));
+            }
+        } 
+    }
+}
