bf622d3f444cf5552bfeb207aabdf283199a4071
Ermin Obradovac <au592117@uni.au.dk>
2020-10-25 15:30:09 +0100

    DataBinding works!

---
+++ Gæld/Gæld/Mvvm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace Gæld
+{
+    class Mvvm : BindableBase
+    {
+        public ICommand _addNew;
+
+        public ICommand addNew
+        {
+            get
+            {
+                return _addNew ?? (_addNew = new DelegateCommand(() =>
+                {
+                    MessageBox.Show("Yeeeha");
+                }));
+            }
+        }
+    }
+}
