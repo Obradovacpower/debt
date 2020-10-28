76dc331f95becabcabf098f7ac9834a9a37bb0e9
Ermin Obradovac <au592117@uni.au.dk>
2020-10-28 15:18:55 +0100

    Yeee

--- Gæld/Gæld/Mvvm.cs
+++ Gæld/Gæld/Mvvm.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using Gæld.Database.Models;
 using Prism.Commands;
 using Prism.Mvvm;
 
@@ -12,7 +14,34 @@ namespace Gæld
 {
     class Mvvm : BindableBase
     {
+        public static ObservableCollection<Person> persons = new ObservableCollection<Person>();
+
         public ICommand _addNew;
+
+        private Person _currentPerson = null;
+        private int _currentIndex = 0;
+
+        public ObservableCollection<Person> Persons
+        {
+            get { return persons; }
+            set
+            {
+                SetProperty(ref persons, value);
+            }
+        }
+
+        public Person CurrentPerson
+        {
+            get { return _currentPerson; }
+            set { SetProperty(ref _currentPerson, value); }
+        }
+
+        public int CurrentIndex
+        {
+            get { return _currentIndex; }
+            set { SetProperty(ref _currentIndex, value); }
+        } 
+
 
         public ICommand addNew
         {
