3f58287c33d22c668dcf34c5e27e90199a2cf9cf
Sebastian <sebastian@nielsen2u.dk>
2020-10-28 15:43:57 +0100

    new migration

--- Gæld/Gæld/Migrations/20201028144305_newInitial.Designer.cs
+++ Gæld/Gæld/Migrations/20201028144305_newInitial.Designer.cs
@@ -4,14 +4,16 @@ using Gæld.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Gæld.Migrations
 {
     [DbContext(typeof(DebtBookContext))]
-    partial class DebtBookContextModelSnapshot : ModelSnapshot
+    [Migration("20201028144305_newInitial")]
+    partial class newInitial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Gæld/Gæld/Migrations/20201028144305_newInitial.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gæld.Migrations
+{
+    public partial class newInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Debts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2020, 10, 28, 15, 43, 5, 121, DateTimeKind.Local).AddTicks(6322));
+
+            migrationBuilder.UpdateData(
+                table: "Debts",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2020, 10, 28, 15, 43, 5, 123, DateTimeKind.Local).AddTicks(6325));
+
+            migrationBuilder.UpdateData(
+                table: "Debts",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2020, 10, 28, 15, 43, 5, 123, DateTimeKind.Local).AddTicks(6325));
+
+            migrationBuilder.UpdateData(
+                table: "Debts",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Date",
+                value: new DateTime(2020, 10, 28, 15, 43, 5, 123, DateTimeKind.Local).AddTicks(6325));
+
+            migrationBuilder.UpdateData(
+                table: "Debts",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Date",
+                value: new DateTime(2020, 9, 28, 15, 43, 5, 123, DateTimeKind.Local).AddTicks(6325));
+
+            migrationBuilder.UpdateData(
+                table: "Debts",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Date",
+                value: new DateTime(2020, 10, 13, 15, 43, 5, 123, DateTimeKind.Local).AddTicks(6325));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Debts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2020, 10, 25, 15, 31, 26, 700, DateTimeKind.Local).AddTicks(8737));
+
+            migrationBuilder.UpdateData(
+                table: "Debts",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2020, 10, 25, 15, 31, 26, 702, DateTimeKind.Local).AddTicks(9049));
+
+            migrationBuilder.UpdateData(
+                table: "Debts",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2020, 10, 25, 15, 31, 26, 702, DateTimeKind.Local).AddTicks(9049));
+
+            migrationBuilder.UpdateData(
+                table: "Debts",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Date",
+                value: new DateTime(2020, 10, 25, 15, 31, 26, 702, DateTimeKind.Local).AddTicks(9049));
+
+            migrationBuilder.UpdateData(
+                table: "Debts",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Date",
+                value: new DateTime(2020, 9, 25, 15, 31, 26, 702, DateTimeKind.Local).AddTicks(9049));
+
+            migrationBuilder.UpdateData(
+                table: "Debts",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Date",
+                value: new DateTime(2020, 10, 10, 15, 31, 26, 702, DateTimeKind.Local).AddTicks(9049));
+        }
+    }
+}
